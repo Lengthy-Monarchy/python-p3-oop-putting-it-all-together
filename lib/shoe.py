83b8cf4c87e7ebf9833a6fdcb90196962dcdbc60
Lengthy_monarchy <anneirungu07@gmail.com>
2024-04-05 10:03:43 +0300

    fixed some of the failing tests

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,11 +1,21 @@
 class Shoe:
     def __init__(self, brand, size):
         self.brand = brand
-        if not isinstance(size, int):
+        self.size = size
+        self.condition = "Old"  # Default condition
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
             print("size must be an integer")
+            self._size = 0  
         else:
-            self.size = size
+            self._size = value
     
     def cobble(self):
-        print("The shoe has been repaired.")
+        print("Your shoe is as good as new!")
         self.condition = "New"
